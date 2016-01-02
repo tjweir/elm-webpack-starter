530e85996bd1c57cf03bf9d4d792d4e03ae2bb45
Tyler Weir <tyler.weir@gmail.com>
2016-01-02 14:04:17 -0500

    Add FTL button to reduce exclamation points

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,6 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing ( onClick )
-import String
 
 -- official 'Elm Architecture' package
 -- https://github.com/evancz/start-app
@@ -12,38 +11,44 @@ import Components.Hello exposing ( hello )
 
 
 -- APP KICK OFF!
+main : Signal Html
 main =
   StartApp.start { model = model, view = view, update = update }
 
 
 -- MODEL
+model : number
 model = 0
-
 
 -- VIEW
 -- Examples of:
 -- 1)  an externally defined component ('hello', takes 'model' as arg)
 -- 2a) styling through CSS classes (external stylesheet)
 -- 2b) styling using inlne style attribute (two variants)
+view : Signal.Address Action -> Int -> Html
 view address model =
   div
     [ class "mt-palette-accent", style styles.wrapper ]
     [
       hello model,
       p [ style [( "color", "#FFF")] ] [ text ( "Elm Webpack Starter" ) ],
-      button [ class "mt-button-sm", onClick address Increment ] [ text "FTW!" ]
+      button [ class "mt-button-sm", onClick address Increment ] [ text "FTW" ],
+      button [ class "mt-button-sm", onClick address Decrement ] [ text "FTL" ]
     ]
 
 
 -- UPDATE
-type Action = Increment
+type Action = Increment | Decrement
 
+update : Action -> number -> number
 update action model =
   case action of
     Increment -> model + 1
+    Decrement -> model - 1
 
 
 -- CSS STYLES
+styles : { wrapper : List ( String, String ) }
 styles =
   {
     wrapper =
