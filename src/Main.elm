d50ad08b8c7f040a827e4ce4df6c283cf41904a5
Tyler Weir <tyler.weir@gmail.com>
2016-01-02 14:48:15 -0500

    Add a tiny list component

--- src/Main.elm
+++ src/Main.elm
@@ -2,13 +2,15 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing ( onClick )
 
+
 -- official 'Elm Architecture' package
 -- https://github.com/evancz/start-app
 import StartApp.Simple as StartApp
 
+
 -- component import example
 import Components.Hello exposing ( hello )
-
+import Components.PersonLists exposing (personlist)
 
 -- APP KICK OFF!
 main : Signal Html
@@ -19,6 +21,7 @@ main =
 -- MODEL
 model : number
 model = 0
+
 
 -- VIEW
 -- Examples of:
@@ -31,9 +34,10 @@ view address model =
     [ class "mt-palette-accent", style styles.wrapper ]
     [
       hello model,
-      p [ style [( "color", "#FFF")] ] [ text ( "Elm Webpack Starter" ) ],
+      p [ style [( "color", "#000000")] ] [ text ( "Elm Webpack Starter" ) ],
       button [ class "mt-button-sm", onClick address Increment ] [ text "FTW" ],
-      button [ class "mt-button-sm", onClick address Decrement ] [ text "FTL" ]
+      button [ class "mt-button-sm", onClick address Decrement ] [ text "FTL" ],
+      personlist model
     ]
 
 
@@ -44,7 +48,10 @@ update : Action -> number -> number
 update action model =
   case action of
     Increment -> model + 1
-    Decrement -> model - 1
+    Decrement -> if model == 0 then
+                   0
+                 else
+                   model - 1
 
 
 -- CSS STYLES
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@
 require( '../dist/index.html' );
 
 // pull in desired CSS/SASS files
-require( './styles/mt.css/mt.scss' );
-require( './styles/app.css' );
+//require( './styles/app.css' );
 
 var Elm = require( './Main' );
 Elm.embed( Elm.Main, document.getElementById( 'main' ) );
